599994caf222acaf705c1c5c835542d1715d77a9
1ch1m0n <2021816818@student.uitm.edu.my>
2023-12-15 16:09:36 +0800

    New

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-/*Hide Content */
-function showContent(contentId) {
-    hideAllContent();
-    var content = document.getElementById(contentId);
-    content.style.maxHeight = content.scrollHeight + "px";
-}
-
 function hideAllContent() {
     var contents = document.getElementsByClassName('content');
     for (var i = 0; i < contents.length; i++) {
         contents[i].style.maxHeight = '0';
     }
-    
-     // Hide nested buttons when hiding all content
-     var nestedButtons = document.querySelector('.nested-buttons');
-     if (nestedButtons) {
-         nestedButtons.style.maxHeight = '0';
+
+    // Hide nested buttons when hiding all content
+    var nestedButtons = document.getElementById('education');
+    if (nestedButtons) {
+        nestedButtons.style.maxHeight = '0';
+    }
 }
