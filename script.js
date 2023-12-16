abb998a259aa26eadd6e6938d6b01cd16eaf40a3
1ch1m0n <2021816818@student.uitm.edu.my>
2023-12-16 23:50:11 +0800

    New

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ function hideAllContent() {
         contents[i].style.maxHeight = '0';
     }
 }
+
+function showContent(sectionId) {
+    var content = document.getElementById(sectionId);
+
+    // Toggle the visibility of the content
+    if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+    } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+    }
+
+    // Prevent the default behavior of the click event
+    event.preventDefault();
+}
