c4c24ee8fb11dcd96f2a1cae93eb20564b382086
1ch1m0n <2021816818@student.uitm.edu.my>
2023-12-17 00:26:18 +0800

    New

--- script.js
+++ script.js
@@ -3,11 +3,13 @@ function showContent(contentId) {
     var content = document.getElementById(contentId);
     content.style.maxHeight = content.scrollHeight + "px";
 
-     // Additional logic to handle specific cases
-     if (contentId === 'ctf') {
+    // Additional logic to handle specific cases
+    if (contentId === 'ctf') {
         showContent('ctf1');
+    } else if (contentId === 'ctf1') { // Fix the typo here
+        // Handle 'ctf1' case if needed
     } else if (contentId === 'ctf2') {
-        showContent('ctf2');
+        // Handle 'ctf2' case if needed
     }
 }
 
