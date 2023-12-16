012b6734f8c6d07b0ad65e214a286a7a20f39d98
1ch1m0n <2021816818@student.uitm.edu.my>
2023-12-17 00:48:52 +0800

    New

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Add interactive features if needed
+// Add interactive features if needed
+document.getElementById('btnAnimate').addEventListener('click', function() {
+    animateButton();
+});
+
+function animateButton() {
+    var button = document.getElementById('btnAnimate');
+    button.classList.add('animated');
+    setTimeout(function() {
+        button.classList.remove('animated');
+    }, 1000);
+}
