fad563a4d2fbf537f764a0bd4d13dd3db36124c1
1ch1m0n <2021816818@student.uitm.edu.my>
2023-12-17 00:03:13 +0800

    New

--- script.js
+++ script.js
@@ -10,10 +10,3 @@ function hideAllContent() {
         contents[i].style.maxHeight = '0';
     }
 }
-
-// Add functions for CTF 1 and CTF 2
-function showContent(contentId) {
-    hideAllContent();
-    var content = document.getElementById(contentId);
-    content.style.maxHeight = content.scrollHeight + "px";
-}
